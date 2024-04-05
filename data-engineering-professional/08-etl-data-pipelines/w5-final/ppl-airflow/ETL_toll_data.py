15b623353a756c726417d383cc4570c8cfee6f2a
Hoon Lee <yl.hoony@gmail.com>
2024-04-05 03:40:16 -0700

    updated

--- data-engineering-professional/08-etl-data-pipelines/w5-final/ppl-airflow/ETL_toll_data.py
+++ data-engineering-professional/08-etl-data-pipelines/w5-final/ppl-airflow/ETL_toll_data.py
@@ -1,12 +1,13 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
 from datetime import datetime as dt
 from datetime import timedelta
 
 # Task 1.1 - Define DAG arguments
 default_args = {
     'owner': 'Hoon Lee',
-    'start_date': dt.today(),
+    'start_date': days_ago(0),
     'email': ['hoonlee@somemail.com'],
     'email_on_failure': True,
     'email_on_retry': True,
@@ -16,7 +17,7 @@ default_args = {
 
 # Task 1.2 - Define the DAG
 dag = DAG(
-    'ETL_toll_data',
+    dag_id='ETL_toll_data',
     schedule_interval=timedelta(days=1),
     default_args=default_args,
     description='Apache Airflow Final Assignment'
@@ -25,7 +26,7 @@ dag = DAG(
 # Task 1.3 - Create a task to unzip data
 unzip_data = BashOperator(
     task_id='unzip_data',
-    bash_command='tar -xvf ./tolldate.tgz -C ./staging',
+    bash_command="tar -xvzf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment/staging",
     dag=dag
 )
 
@@ -36,7 +37,7 @@ unzip_data = BashOperator(
 # and save them into a file named csv_data.csv.
 extract_data_from_csv = BashOperator(
     task_id='extract_data_from_csv',
-    bash_commands='cut -d "," -f1-4  ./staging/vehicle-data.csv > ./staging/csv_data.csv',
+    bash_command="cut -d ',' -f1-4  /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv > /home/project/airflow/dags/finalassignment/staging/csv_data.csv",
     dag=dag
 )
 
@@ -47,7 +48,7 @@ extract_data_from_csv = BashOperator(
 # and save it into a file named tsv_data.csv.
 extract_data_from_tsv = BashOperator(
     task_id='extract_data_from_tsv',
-    bash_commands='cut -f5-7  ./staging/tollplaza-data.tsv | tr "\t" "," | tr -d "\r" > ./staging/tsv_data.csv',
+    bash_command="cut -f5-7 /home/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv | tr '\\t' ',' | sed 's/\\r$//' > /home/project/airflow/dags/finalassignment/staging/tsv_data.csv",
     dag=dag
 )
 
@@ -58,14 +59,14 @@ extract_data_from_tsv = BashOperator(
 # and save it into a file named fixed_width_data.csv.
 extract_data_from_fixed_width = BashOperator(
     task_id='extract_data_from_fixed_width',
-    bash_commands="cut -b 59-67 ./staging/payment-data.txt | tr ' ' ',' > ./staging/fixed_width_data.csv",
+    bash_command="cut -b 59-68 /home/project/airflow/dags/finalassignment/staging/payment-data.txt | tr ' ' ',' > /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv",
     dag=dag
 )
 
 # Task 1.7 - Create a task to consolidate data extracted from previous tasks
 consolidate_data = BashOperator(
     task_id='consolidate_data',
-    bash_commands='paste -d "," ./staging/csv_data.csv ./staging/tsv_data.csv ./staging/fixed_width_data.csv > ./staging/extracted_data.csv',
+    bash_command="paste -d ',' /home/project/airflow/dags/finalassignment/staging/csv_data.csv /home/project/airflow/dags/finalassignment/staging/tsv_data.csv /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv > /home/project/airflow/dags/finalassignment/staging/extracted_data.csv",
     dag=dag
 )
 
@@ -74,9 +75,15 @@ consolidate_data = BashOperator(
 # and save it into a file named transformed_data.csv in the staging directory.
 transform_data = BashOperator(
     task_id='transform_data',
-    bash_commands="awk 'BEGIN {FS=OFS=','} { $4 = toupper($4) } 1' ./staging/extracted_data.csv > ./staging/transformed_data.csv",
+    bash_command="awk 'BEGIN {FS=OFS=\",\"} { $4 = toupper($4) } 1' /home/project/airflow/dags/finalassignment/staging/extracted_data.csv > /home/project/airflow/dags/finalassignment/staging/transformed_data.csv",
     dag=dag
 )
+
+# transform_data = BashOperator(
+#     task_id='transform_data',
+#     bash_command='tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/finalassignment/staging/extracted_data.csv > /home/project/airflow/dags/finalassignment/staging/transformed_data.csv',
+#     dag=dag
+# )
 
 # Task 1.9 - Define the task pipeline
 unzip_data \
@@ -85,3 +92,4 @@ unzip_data \
     >> extract_data_from_fixed_width \
     >> consolidate_data \
     >> transform_data
+
